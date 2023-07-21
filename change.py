4a73ffab96e1ccb13603abb690c2d274abb88787
gpark608 <46937752+gpark608@users.noreply.github.com>
2023-07-21 15:06:58 -0600

    Update change.py

--- change.py
+++ change.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-today = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+today = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
 with open("blank.txt", "a+") as myfile:
     myfile.write(f"Today's Date is {today}\n")
     myfile.seek(0)
