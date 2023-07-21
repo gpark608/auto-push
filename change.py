b339fec79a900aecddb11a8eb42a0b27b4e844c8
gpark608 <gpark608>
2023-07-21 13:05:05 -0600

    test

--- change.py
+++ change.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 today = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-with open("blank.txt", "a") as myfile:
-    myfile.write(f"\nToday's Date in UTC is {today}")
-    
+with open("blank.txt", "a+") as myfile:
+    myfile.write(f"Today's Date in UTC is {today}\n")
+    myfile.seek(0)
+    print(myfile.read())
